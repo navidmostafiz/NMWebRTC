7c1db6f2f32c82a859b37b6e314e2ff8b9d1c433
navidmostafiz <navidmostafiz@gmail.com>
2020-07-05 01:24:18 -0400

    port fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require("http").Server(app);
 var PORT = process.env.PORT || 5000;
 
 const { PeerServer } = require("peer");
-const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+const peerServer = PeerServer({ port: PORT, path: "/myapp" });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
